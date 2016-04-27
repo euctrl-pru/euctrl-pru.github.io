70a38d93d8ac89d2b0413cb90e281a2f83f8e5a4
espinielli <enrico.spinielli@eurocontrol.int>
2016-04-27 15:52:57 +0000

    Updating to euctrl-pru/euctrl-pru.github.io@2f26f4771a53f3e0641de1fe7238b4f8e4f39f64, tagged .

---
+++ euctrl-pru.github.io/euctrl-pru.github.io/euctrl-pru.github.io/euctrl-pru.github.io/assets/tbl_ertSU_YY-e043dca469d9e70e0d8e85d9548604ec.js
@@ -0,0 +1,1 @@
+!function(){function e(){var e=new google.visualization.ChartWrapper({chartType:"Table",containerId:"tbl_ertSU_YY",dataSourceUrl:"https://docs.google.com/spreadsheets/d/1txmPuoLLfCuuQdWSQfBZmQWPEHD__MVu-Y5xsvWCO3Y/edit?usp=sharing&alt=json&sheet=ERT_SU_CZ&range=A1:D41&headers=1",options:{width:550,height:440},view:{columns:[0,1,2,3]}});e.draw()}google.setOnLoadCallback(e)}();
